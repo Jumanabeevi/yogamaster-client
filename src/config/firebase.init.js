e81bd14d90f81e7c70f6b718b172ae4c434aeaeb
jumanabeevi <jumanabeevi.2@gmail.com>
2024-12-08 16:28:04 +0530

    first commit

---
+++ src/config/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqmwLzSFV4wC6rriwg7uGkTJ3vl3RLKQw",
+  authDomain: "yogamaster-project.firebaseapp.com",
+  projectId: "yogamaster-project",
+  storageBucket: "yogamaster-project.firebasestorage.app",
+  messagingSenderId: "273660235796",
+  appId: "1:273660235796:web:b5cb19baf16b9e40ef5701"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
